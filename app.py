a1dc9a23f5fb26271e32f425ab8ae45e60401c8a
Sean Behan <inbox@seanbehan.com>
2015-06-14 23:38:35 -0400

    stuff

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request, render_template, json
-from lib import search_places
+from lib import search_places, save_comment
+from flask_request_params import bind_request_params
 
 app = Flask(__name__)
+app.before_request(bind_request_params)
 
 @app.route("/")
 def home():
-    return "Welcome"
+    return render_template("home.html")
 
 @app.route("/places.json")
 def places():
@@ -16,16 +18,16 @@ def places():
 
 @app.route("/places/<place_id>.json")
 def place(place_id):
-    return json.dumps(search_places(place_id=place_id))
-
-@app.route("/places/<place_id>/comments.json")
-def place_comment(place_id):
-    comment = request.data.get('comment')
-
-    if comment:
-        db['comments'].insert(comment)
-
     return jsonify(search_places(place_id=place_id))
+
+@app.route("/places/<place_id>/comments.json", methods=["POST"])
+def place_comment(place_id):
+    comment_params = dict(request.params['comment'])
+    comment_params['place_id'] = place_id
+
+    comment = save_comment(comment_params)
+
+    return jsonify(comment)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- lib.py
+++ lib.py
@@ -2,9 +2,16 @@ import requests
 from urllib import urlencode
 import json
 import dataset
+import os
 
-# os.environ.get('DATABASE_URL')
-db = dataset.connect('sqlite:///development.db')
+if os.path.exists(".env"):
+    for row in filter(None, open(".env").read().strip().split("\n")):
+        if row.startswith('#'):
+            continue
+        row = row.split("=")
+        os.environ[row[0]]=row[1]
+
+db = dataset.connect(os.environ['DATABASE_URL'])
 
 def search_places(lat=None, lng=None, place_id=None):
     if place_id:
@@ -13,10 +20,11 @@ def search_places(lat=None, lng=None, place_id=None):
     return [dict(name=p['name'], place_id=p['place_id']) for p in google_places(lat=lat, lng=lng)['results']]
 
 def get_place(place_id):
-    '''
-    return place from database or google places api
-    '''
-    place = db['places'].find_one(place_id=place_id)
+    place = dict(db['places'].find_one(place_id=place_id))
+    place['comments'] = []
+
+    if place:
+        place['comments'] = [r for r in db.query('select * from comments where place_id = :place_id order by id desc', place_id=place_id)]
 
     if not place:
         result = google_place(place_id)['result']
@@ -26,8 +34,13 @@ def get_place(place_id):
 
     return place
 
+def save_comment(comment):
+    if comment:
+        db['comments'].insert(comment)
+    return comment
+
 PARAMS = dict(
-        key="AIzaSyA2t1lVBkuAWzSZuiymfYZ1xOQ1XKMhiqc",
+        key=os.environ['GOOGLE_API_KEY'],
         rankby="distance",
         types="accounting|airport|amusement_park|aquarium|art_gallery|atm|bakery|bank|bar|beauty_salon|bicycle_store|book_store|bowling_alley|bus_station|cafe|campground|car_dealer|car_rental|car_repair|car_wash|casino|cemetery|church|city_hall|clothing_store|convenience_store|courthouse|dentist|department_store|doctor|electrician|electronics_store|embassy|establishment|finance|fire_station|florist|food|funeral_home|furniture_store|gas_station|general_contractor|grocery_or_supermarket|gym|hair_care|hardware_store|health|hindu_temple|home_goods_store|hospital|insurance_agency|jewelry_store|laundry|lawyer|library|liquor_store|local_government_office|locksmith|lodging|meal_delivery|meal_takeaway|mosque|movie_rental|movie_theater|moving_company|museum|night_club|painter|park|parking|pet_store|pharmacy|physiotherapist|place_of_worship|plumber|police|post_office|real_estate_agency|restaurant|roofing_contractor|rv_park|school|shoe_store|shopping_mall|spa|stadium|storage|store|subway_station|synagogue|taxi_stand|train_station|travel_agency|university|veterinary_care|zoo",
         sensor="true",
